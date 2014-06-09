ad63a55e9570bf52a0ddad5e09fcffdcd4b98b03
Anthony Sottile <asottile@umich.edu>
2014-06-09 09:53:07 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+[![Build Status](https://travis-ci.org/pre-commit/pre-commit-mirror-maker.svg?branch=master)](https://travis-ci.org/pre-commit/pre-commit-mirror-maker)
+[![Coverage Status](https://img.shields.io/coveralls/pre-commit/pre-commit-mirror-maker.svg?branch=master)](https://coveralls.io/r/pre-commit/pre-commit-mirror-maker)
+
+pre-commit-mirror-maker
+==========
+
+Scripts for creating mirror repositories that do not have hooks.yaml
+
+
+### Installation
+
+`$ pip install pre-commit-mirror-maker`
+
+
+### Sample Usage
+
+TODO
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='pre-commit-mirror-maker',
+    description="Scripts for creating mirror repositories that do not have hooks.yaml",
+    url='https://github.com/pre-commit/pre-commit-mirror-maker',
+    version='0.0.0',
+
+    author='Anthony Sottile',
+    author_email='asottile@umich.edu',
+
+    platforms='all',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    packages=find_packages(exclude=('tests*', 'testing*')),
+    install_requires=[],
+)
