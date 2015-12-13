f8e82739eaea981177c4acfbe03c64a83812eeda
Anthony Sottile <asottile@umich.edu>
2015-12-13 13:57:21 -0800

    v0.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='pre-commit-mirror-maker',
     description="Scripts for creating mirror repositories that do not have hooks.yaml",
     url='https://github.com/pre-commit/pre-commit-mirror-maker',
-    version='0.2.0',
+    version='0.3.0',
 
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
